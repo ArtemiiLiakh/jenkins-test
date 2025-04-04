108c18b23c36c7ce42763cea033bd1361d7b345e
ArtemLyah <asuskontopo@gmail.com>
2025-04-04 15:29:54 +0300

    update

--- tests/e2e/UserRouter.test.ts
+++ tests/e2e/UserRouter.test.ts
@@ -16,30 +16,30 @@ describe('User router tests', () => {
     user = await userUtils.create();
   });
 
-  // test('Test user creation', async () => {
-  //   const dto = {
-  //     name: 'name',
-  //     username: 'username',
-  //   } as UserCreateDTO;
+  test('Test user creation', async () => {
+    const dto = {
+      name: 'name',
+      username: 'username',
+    } as UserCreateDTO;
     
-  //   const res = await supertest(app)
-  //     .post('/api/users')
-  //     .send(dto)
-  //     .expect(201);
+    const res = await supertest(app)
+      .post('/api/users')
+      .send(dto)
+      .expect(201);
     
-  //   expect(res.body.user.name).toEqual(dto.name);
-  //   expect(res.body.user.username).toEqual(dto.username);
-  // });
+    expect(res.body.user.name).toEqual(dto.name);
+    expect(res.body.user.username).toEqual(dto.username);
+  });
 
-  // test('Test getting user by id', async () => {
-  //   const res = await supertest(app)
-  //     .get(`/api/users/${user.id}`)
-  //     .expect(200);
+  test('Test getting user by id', async () => {
+    const res = await supertest(app)
+      .get(`/api/users/${user.id}`)
+      .expect(200);
 
-  //   expect(res.body.user.id).toEqual(user.id);
-  //   expect(res.body.user.name).toEqual(user.name);
-  //   expect(res.body.user.username).toEqual(user.username);
-  // });
+    expect(res.body.user.id).toEqual(user.id);
+    expect(res.body.user.name).toEqual(user.name);
+    expect(res.body.user.username).toEqual(user.username);
+  });
 
   test('Test user updating', async () => {
     const dto = {
